15df6be4ee7b4b9563a4b60edc652c3ad9b56fa8
zein3 <zeinhaddad02@gmail.com>
2022-01-10 13:29:02 +0700

    prepare for deployment

--- index.js
+++ index.js
@@ -9,10 +9,11 @@ const io = require("socket.io")(server, {
     methods: ["GET", "POST"]
   }
 })
-const PORT = process.env.PORT || 5000;
 
 require('dotenv').config();
+const PORT = process.env.PORT || 5000;
 app.use(cors());
+app.use('/', express.static('public'));
 
 io.on('connection', (socket) => {
   socket.on('message', (message) => {
